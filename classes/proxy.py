11364c75478d41e8743795e7dc089243f030229a
ParrotDevelopers <72994504+ParrotDevelopers@users.noreply.github.com>
2023-01-22 14:03:53 +0100

    proxyfix

--- classes/proxy.py
+++ classes/proxy.py
@@ -13,30 +13,8 @@ class Proxy:
         self.proxy = proxy if proxy else p
         self.useproxy = getSetting("useProxy").lower() == 'true'
 
-    def json(self, test_ = False):
-        if test_ == False:
-            if self.useproxy == False:
-                return {}
-
+    def json(self):
         return {
             'http': f"{self.protocol}://{self.proxy}",
             'https': f"{self.protocol}://{self.proxy}"
         }
-
-    def test(self):
-        try:
-            resp = requests.get(
-                "https://fast.com/",
-                headers=Firefox().headers,
-                proxies=self.json(test_=True),
-                timeout=30,
-                verify=False
-            )
-        except Exception as e: 
-            print("Proxy didnt work, reason:\n", e, sep="")
-            return False
-
-        if resp.status_code in range(200, 400):
-            return True
-        return False
-        
