c5829e2d4da8392f47410a60be60799370cb2cd6
ParrotDevelopers <72994504+ParrotDevelopers@users.noreply.github.com>
2023-01-23 14:44:54 +0100

    log viewer

--- routes/admin.py
+++ routes/admin.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template
+from utils.paths import DB_FOLDER
+import os
 
 admin = Blueprint("admin", __name__)
+logFile = os.path.join(DB_FOLDER, "app.log")
 
 @admin.route('/')
 def index():
@@ -9,3 +12,8 @@ def index():
 @admin.route('/terminal')
 def terminal():
     return render_template("terminal.html")
+
+@admin.route('/log')
+def log():
+    return render_template("log.html", lines=open(logFile, "r").read().split("\n"))
+
